1057a0779cc5c6c75404146654a18ec11b7c39e8
Lívia Rocha <liviaraj93@gmail.com>
2022-07-06 21:21:06 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const mario = document.querySelector(".super-mario");
+const pipe = document.querySelector(".pipe-game");
+
+const jump = () => {
+    mario.classList("jump-mario");
+
+    setTimeout(() => {
+        mario.classList.remove("jump-mario");
+
+    }, 500)
+}
+
+const loopGame = setInterval(() => {
+
+    pipe.style.animation = "none";
+    pipe.style.left = `${pipePosition}px`;
+
+    mario.style.animation = "none";
+    mario.style.bottom = `${marioPosition}px`;
+
+    mario.src = "./assets/mario-game-over.png";
+    mario.style.width = "75px";
+    mario.style.marginLeft = "45px";
+
+
+    clearInterval(loopGame);
+
+
+})
+
+document.addEventListener("keydown", jump);
